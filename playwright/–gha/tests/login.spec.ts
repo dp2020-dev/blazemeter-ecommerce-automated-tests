a58e62fd557e0c663eb67f5fdf5eece3564baa28
Daniel Phillips <ebost2004@yahoo.co.uk>
2023-10-11 19:54:31 +0100

    Log in tests (happy path and failed log in)

--- playwright/–gha/tests/login.spec.ts
+++ playwright/–gha/tests/login.spec.ts
@@ -2,28 +2,43 @@ import { test, expect, Page } from "@playwright/test";
 test.beforeEach(async ({ page }) => {
   await page.goto("https://www.demoblaze.com/");
 });
+
+test("Succesful log in", async ({ page }) => {
+  await page.getByRole("link", { name: "Log in" }).click();
+  await page.locator("#loginusername").click();
+  await page.locator("#loginusername").fill("test");
+  await page.locator("#loginusername").press("Tab");
+  await page.locator("#loginpassword").fill("test");
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await page.getByRole("link", { name: "Welcome test" }).click();
+});
+
 test.describe("Log in tests", () => {
   test("Verify Login Error Message", async ({ page }) => {
+    const userErrorText = "User does not exist."; //note- cannot verify this text, i.e. incorrect text still passes?
+
     await page.getByRole("link", { name: "Log in" }).click();
     await page.locator("#loginusername").click();
     await page.locator("#loginusername").fill("notuser@demo.com");
     await page.locator("#loginusername").press("Tab");
     await page.locator("#loginpassword").fill("notPassword");
-    page.once("dialog", (dialog) => {
-      console.log(`Dialog message: ${dialog.message()}`);
-      dialog.dismiss().catch(() => {});
+
+    const locator = page.locator("#loginpassword");
+    await expect(locator).toHaveValue("notPassword");
+
+    const locatorUsername = page.locator("#loginusername");
+    await expect(locatorUsername).toHaveValue("notuser@demo.com");
+
+    await page.getByRole("button", { name: "Log in" }).click();
+
+    page.on("dialog", async (alert) => {
+      const text = alert.message();
+      console.log(text);
+      await expect(text).toMatch(userErrorText);
+      await alert.accept();
+
+      page.getByRole("link", { name: "Log in" });
     });
-    await page.getByRole("button", { name: "Log in" }).click();
-  });
-
-  test("Succesful log in", async ({ page }) => {
-    await page.getByRole("link", { name: "Log in" }).click();
-    await page.locator("#loginusername").click();
-    await page.locator("#loginusername").fill("test");
-    await page.locator("#loginusername").press("Tab");
-    await page.locator("#loginpassword").fill("test");
-    await page.getByRole("button", { name: "Log in" }).click();
-
-    await page.getByRole("link", { name: "Welcome test" }).click();
   });
 });
