bd948b13db6fba36ad86e23df5de5267d61b20e2
Daniel Phillips <ebost2004@yahoo.co.uk>
2023-11-05 15:58:57 +0000

    Fixed 'next' button selector

--- playwright/tests/shopping.spec.ts
+++ playwright/tests/shopping.spec.ts
@@ -9,7 +9,41 @@ test.describe("Online shopping", () => {
     await page.waitForSelector("#carouselExampleIndicators", {
       state: "visible",
     });
-    //navigate to basket -basket oage with methods
-    //check no. & value of items
+
+    // await page.locator("div:nth-child(5) > .card > a").click();
+    await page.getByRole("link", { name: "Iphone 6 32gb" }).click();
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await page.getByRole("link", { name: "Add to cart" }).click();
+    await page.getByRole("link", { name: "Home (current)" }).click();
+    await page.locator("div:nth-child(8) > .card > a").click();
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await page.getByRole("link", { name: "Add to cart" }).click();
+    await page.getByRole("link", { name: "Home (current)" }).click();
+
+    const nextButton = await page.locator('button:has-text("Next")');
+    await nextButton.click();
+
+    await page.waitForSelector("#carouselExampleIndicators", {
+      state: "visible",
+    });
+    await page.locator("div:nth-child(11");
+    await page.getByRole("link", { name: "2017 Dell 15.6 Inch" }).click();
+    // await page.locator("div:nth-child(13) > .card > a").click();
+    page.once("dialog", (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      dialog.dismiss().catch(() => {});
+    });
+    await page.getByRole("link", { name: "Add to cart" }).click();
+    await page.getByRole("link", { name: "Cart", exact: true }).click();
   });
+
+  //navigate to basket -basket page with methods
+  //check no. & value of items
 });
+// });
